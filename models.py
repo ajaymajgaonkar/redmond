950d1bf01bc420cc5741db8cc443a89c84130afd
Ajay Majgaonkar <amajgaon@amazon.com>
2017-01-02 21:50:47 -0800

    Updating models and requirements.txt

--- models.py
+++ models.py
@@ -1,16 +1,12 @@
 class Candidate:
-
-	Experience projects = []
-	
-	def __init__(self):
+	def __init__(self, name, email, summary, projects):
 		self.name = name
 		self.email = email
 		self.summary = summary
 		self.projects = projects
 
 class Experience:
-
-	def __init__(self):
+	def __init__(self, company, duration, description):
 		self.company = company
 		self.duration = duration
 		self.description = description
