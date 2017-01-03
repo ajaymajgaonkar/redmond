2938878065bd3a012f253a5288b7a7761c0d1d93
ajaymajgaonkar <ajay.majgaonkar@gmail.com>
2017-01-02 19:10:48 -0800

    Adding simple model classes

---
+++ models.py
@@ -0,0 +1,16 @@
+class Candidate:
+
+	Experience projects = []
+	
+	def __init__(self):
+		self.name = name
+		self.email = email
+		self.summary = summary
+		self.projects = projects
+
+class Experience:
+
+	def __init__(self):
+		self.company = company
+		self.duration = duration
+		self.description = description
