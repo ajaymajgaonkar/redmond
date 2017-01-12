a4680e80373fe448eeaddac5bc1a808ac0c9b19a
ajaymajgaonkar <ajay.majgaonkar@gmail.com>
2017-01-11 20:23:44 -0800

    Updating to use wtforms 3.0

--- forms.py
+++ forms.py
@@ -1,25 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms import validators
-from wtforms.validators import Required, Email
+from wtforms.validators import Email, DataRequired
 
 
 class SignupForm(FlaskForm):
-    first_name = StringField("First Name", validators=[Required("First name is required")])
-    last_name = StringField("Last Name", validators=[Required("Last name is required")])
-    email = StringField("Email", validators=[Required("Email is required"), Email("Please enter valid email address")])
-    password = PasswordField("Password", validators=[Required("Password is required")])
+    first_name = StringField("First Name", validators=[DataRequired("First name is required")])
+    last_name = StringField("Last Name", validators=[DataRequired("Last name is required")])
+    email = StringField("Email", validators=[DataRequired("Email is required"), Email("Please enter valid email address")])
+    password = PasswordField("Password", validators=[DataRequired("Password is required")])
     submit = SubmitField("Sign Up")
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Name", validators=[Required("Please specify your name")])
-    email = StringField("Email", validators=[Required("Email is required"), Email("Please enter valid email address")])
-    message = TextAreaField("Message", validators=[Required("Please write some message")])
+    name = StringField("Name", validators=[DataRequired("Please specify your name")])
+    email = StringField("Email", validators=[DataRequired("Email is required"), Email("Please enter valid email address")])
+    message = TextAreaField("Message", validators=[DataRequired("Please write some message")])
     submit = SubmitField("Send")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[Required("Email is required"), Email("Please enter valid email address")])
-    password = PasswordField("Password", validators=[Required("Password is required")])
+    email = StringField("Email", validators=[DataRequired("Email is required"), Email("Please enter valid email address")])
+    password = PasswordField("Password", validators=[DataRequired("Password is required")])
     submit = SubmitField("Login")
